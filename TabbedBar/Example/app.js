85779a9dfc1010101cf487b245c30da608db2c95
Marco Cancellieri <marco@clever-apps.com>
2014-06-26 11:10:02 +0200

    cleaned example

--- TabbedBar/Example/app.js
+++ TabbedBar/Example/app.js
@@ -1,40 +1,47 @@
 	var win = Ti.UI.createWindow({
-		backgroundColor:"#ffffff",
-		exitOnClose:true,
+	    backgroundColor: "#ffffff",
+	    exitOnClose: true,
 	});
 
 	var tabbedBar = require("/ui/modules/TabbedBar");
-	
-	var bar = tabbedBar.createTabbedBar({labels:["Tab 1", "Tab 2", "Tab 3", "Tab 4", "Tab 5"], index:0, selectedColor: "#ffffff",tintColor: "#007AFF", top:0, width:"90%"});
-	
-	bar.addEventListener("click", function(e){
-		Ti.API.info(e.index);
-		var animation = Titanium.UI.createAnimation({duration:500});
-		switch(e.index){
-			case 0:
-			animation.backgroundColor="white";
-			win.animate(animation);
-			break;
-			case 1:
-			animation.backgroundColor="red";
-			win.animate(animation);
-			break;
-			case 2:
-			animation.backgroundColor="green";
-			win.animate(animation);
-			break;
-			case 3:
-			animation.backgroundColor="blue";
-			win.animate(animation);
-			break;
-			case 4:
-			animation.backgroundColor="black";
-			win.animate(animation);
-			break;
-		}
+
+	var bar = tabbedBar.createTabbedBar({
+	    labels: ["Tab 1", "Tab 2", "Tab 3", "Tab 4", "Tab 5"],
+	    index: 0,
+	    selectedColor: "#ffffff",
+	    tintColor: "#007AFF",
+	    top: 0,
+	    width: "90%"
 	});
-	
-	
+
+	var animation = Titanium.UI.createAnimation({
+	    duration: 500
+	});
+
+	bar.addEventListener("click", function (e) {
+	    Ti.API.info(e.index);
+
+	    switch (e.index) {
+	    case 0:
+	        animation.backgroundColor = "white";
+	        break;
+	    case 1:
+	        animation.backgroundColor = "red";
+	        break;
+	    case 2:
+	        animation.backgroundColor = "green";
+	        break;
+	    case 3:
+	        animation.backgroundColor = "blue";
+	        break;
+	    case 4:
+	        animation.backgroundColor = "black";
+	        break;
+	    }
+	    win.animate(animation);
+	});
+
+
 	win.add(bar);
-	
+
 	win.open();
